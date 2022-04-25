62cf64498c32a6e085b898da402c816ae982a9a6
zoya1201 <wahidzoya12@gmail.com>
2022-04-25 16:51:06 +0800

    adding my book

---
+++ _build/jupyter_execute/Introduction.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Introduction
+
+# This book is for those students who are interested in fireflies microbit, they can see the examples, codes and steps to learn about the fireflies microbit.
+# This book also includes images, videos and python editor. 
+# There are two examples of fireflies microbit in this book, it can help students to learn fireflies microbit.
+# One example is students can press button A to flash microbit, for that we will need two computers and one power bank.
+#             
+# ![microbit](inn.png)
+
+# # Learning Objective
+
+# - learn microbit by pressing button A
+# - learn microbit by not pressing any button, it will flash itself
+# - learn using codes
+
+# # What you will learn? 
+
+# In[ ]:
+
+
+
+
+
+# - how to use radio communication between micro:bits to trigger events
+# - that the micro:bit's LED display can be made brighter and dimmer
+# - how to use random numbers and chance to mimic real-world behaviour
+
+# # What is it?
+
+# Fireflies are insects that use bioluminescence (light made in their bodies) to communicate with each other. 
+# 
+# Flash the program on at least three micro:bits in the same room. Press button A on any micro:bit and you should see a 'swarm' of micro:bits lighting up as they receive a radio message. It works best in a dark room with lots of micro:bits. 
+# 
+# Once a micro:bit has received the radio message it has a one in ten chance of transmitting its own radio message and starting another wave of glowing lights in the other micro:bits. 
+# 
+# When the lights eventually stop, press button A on any micro:bit to restart the 'swarm' of communication. 
+
+# # What you need? 
+
+# - at least 3 micro:bits, the more the better
+# - micro:bit battery packs (optional)
+
+# ```{note} 
+# you will need
+# - 3 microbit
+# - a battery pack (if necessary)
+# ```
+
+# 
+
+# I have done all the steps and example, it was very interseting and fun, i hope you will also find it interesting and can able to do all the examples clearly
+
+# In[ ]:
+
+
+
+
